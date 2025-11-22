4e0fe3c18fc5ff41eedc0bff9f4cf2b9ec751276
kietxuan <kietnguyenxuan2401@gmail.com>
2025-11-22 13:52:25 +0700

    Update connection.py

--- app/db/connection.py
+++ app/db/connection.py
@@ -2,19 +2,22 @@ import mysql.connector
 import os
 from dotenv import load_dotenv
 
-# Tải biến môi trường từ file .env
 load_dotenv()
 
 def get_db_connection():
-    """Tạo kết nối tới CSDL MySQL và trả về object connection."""
+    print("--- Đang kết nối tới HospitalDB... ---")
     try:
         conn = mysql.connector.connect(
-            host=os.getenv('DB_HOST'),
-            user=os.getenv('DB_USER'),
-            password=os.getenv('DB_PASSWORD'),
-            database=os.getenv('DB_NAME')
+            host='127.0.0.1',
+            port=3306,
+            user='root',
+            password='konu14102006', 
+            
+            database='hospital_patient_manager', 
+            
+            use_pure=True
         )
         return conn
     except mysql.connector.Error as err:
-        print(f"Lỗi kết nối CSDL: {err}")
+        print(f"❌ Lỗi kết nối: {err}")
         return None
